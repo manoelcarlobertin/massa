910483f568e9b7e50b957bb7871cb1297538ef7d
BertinManoelBr <manoelcarlobertin@outlook.com>
2025-08-20 04:46:33 -0300

    Essa associacao com o texto rico, prepara o campo para o formulário e nos da as ferramentas para exibi-lo corretamente.

--- app/models/vaga.rb
+++ app/models/vaga.rb
@@ -1,2 +1,13 @@
 class Vaga < ApplicationRecord
+  has_rich_text :description
 end
+# == Schema Information
+#
+# Table name: vagas
+#
+#  id          :bigint           not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
